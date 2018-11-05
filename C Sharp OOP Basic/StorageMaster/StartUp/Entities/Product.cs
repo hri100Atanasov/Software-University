741b5683981c97bf0900fe6f890eae1b9d92ef9b
Hristo <h_atanasov_@abv.bg>
2018-11-05 13:04:07 +0200

    Exam preparation

--- C Sharp OOP Basic/StorageMaster/StartUp/Entities/Product.cs
+++ C Sharp OOP Basic/StorageMaster/StartUp/Entities/Product.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace StorageMaster.Entities
 {
-    class Product
+    public abstract class Product
     {
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            set {
+                if (value<0)
+                {
+                    throw new InvalidOperationException("Price cannot be negative!");
+                }
+                price = value; }
+        }
+
     }
 }
