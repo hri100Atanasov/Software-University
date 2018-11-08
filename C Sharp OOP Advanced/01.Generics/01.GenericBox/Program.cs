ea0e877865f427c9337c5189126c2f9800c04541
Hristo <hri100atanasov@gmail.com>
2018-11-08 18:47:43 +0200

    update

---
+++ C Sharp OOP Advanced/01.Generics/01.GenericBox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.GenericBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- C Sharp OOP Basic/StorageMaster/StartUp/StartUp.cs
+++ C Sharp OOP Basic/StorageMaster/StartUp/StartUp.cs
@@ -1,6 +1,7 @@
 ﻿using StorageMaster.Core;
 using StorageMaster.Core.IO;
 using StorageMaster.Core.IO.Contracts;
+using StorageMaster.Entities.Vehicles;
 
 namespace StorageMaster
 {
@@ -8,11 +9,12 @@ namespace StorageMaster
     {
         static void Main(string[] args)
         {
-            IWriter writer = new ConsoleWriter();
-            IReader reader = new ConsoleReader();
+            //IWriter writer = new ConsoleWriter();
+            //IReader reader = new ConsoleReader();
 
-            var engine = new Engine(reader, writer);
-            engine.Run();
+            //var engine = new Engine(reader, writer);
+            //engine.Run();
+            System.Console.WriteLine(typeof(Semi));
         }
     }
 }
