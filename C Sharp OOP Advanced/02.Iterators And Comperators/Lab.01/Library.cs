0fd75d3877fbcaf56c76ef6a44f7a216da35f961
Hristo <hri100atanasov@gmail.com>
2018-11-11 22:27:57 +0200

    update

---
+++ C Sharp OOP Advanced/02.Iterators And Comperators/Lab.01/Library.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Library : IEnumerable<Book>
+{
+    private readonly List<Book> books;
+
+    public Library(params Book[] books)
+    {
+        this.books = new List<Book>(books);
+    }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        return new LibraryIterator(books);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    //Nested Class
+    private class LibraryIterator : IEnumerator<Book>
+    {
+        public Book Current => books[index];
+
+        object IEnumerator.Current => Current;
+
+        private int index;
+        private readonly IReadOnlyList<Book> books;
+        public LibraryIterator(IReadOnlyList<Book> books)
+        {
+            this.books = books;
+            index = -1;
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            return ++index < books?.Count;
+        }
+
+        public void Reset()
+        {
+            //throw new NotSupportedException();
+        }
+    }
+}
+
