48229c36f0093de0f12764462ea6d3d3a86e3527
Hristo <hri100atanasov@gmail.com>
2018-11-16 10:39:00 +0200

    Factories using reflection

--- C Sharp OOP Basic/StorageMaster/StartUp/Entities/Factories/StorageFactory.cs
+++ C Sharp OOP Basic/StorageMaster/StartUp/Entities/Factories/StorageFactory.cs
@@ -1,5 +1,7 @@
 ﻿using StorageMaster.Entities.Storages;
 using System;
+using System.Linq;
+using System.Reflection;
 
 namespace StorageMaster.Entities.Factories
 {
@@ -7,17 +9,28 @@ namespace StorageMaster.Entities.Factories
     {
         public Storage CreateStorage(string type, string name)
         {
-            switch (type)
+            //switch (type)
+            //{
+            //    case "AutomatedWarehouse":
+            //        return new AutomatedWarehouse(name);
+            //    case "DistributionCenter":
+            //        return new DistributionCenter(name);
+            //    case "Warehouse":
+            //        return new Warehouse(name);
+            //    default:
+            //        throw new InvalidOperationException("Invalid storage type!");
+            //}
+
+            //Using reflection
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type storageType = assembly.GetTypes().FirstOrDefault(t => t.Name == type && !t.IsAbstract);
+            if (storageType==null&&!typeof(Storage).IsAssignableFrom(storageType))
             {
-                case "AutomatedWarehouse":
-                    return new AutomatedWarehouse(name);
-                case "DistributionCenter":
-                    return new DistributionCenter(name);
-                case "Warehouse":
-                    return new Warehouse(name);
-                default:
-                    throw new InvalidOperationException("Invalid storage type!");
+                throw new InvalidOperationException("Invalid storage type!");
             }
+
+            Storage storage = (Storage)Activator.CreateInstance(storageType, name);
+            return storage;
         }
     }
 }
--- C Sharp OOP Basic/StorageMaster/StartUp/Entities/Factories/VehicleFactory.cs
+++ C Sharp OOP Basic/StorageMaster/StartUp/Entities/Factories/VehicleFactory.cs
@@ -1,23 +1,39 @@
 ﻿using StorageMaster.Entities.Vehicles;
 using System;
+using System.Linq;
+using System.Reflection;
 
 namespace StorageMaster.Entities.Factories
 {
-   public class VehicleFactory
+    public class VehicleFactory
     {
         public Vehicle CreateVehicle(string type)
         {
-            switch (type)
+            //switch (type)
+            //{
+            //    case "Semi":
+            //        return new Semi();
+            //    case "Truck":
+            //        return new Truck();
+            //    case "Van":
+            //        return new Van();
+            //    default:
+            //        throw new InvalidOperationException("Invalid vehicle type!");
+            //}
+
+            //Using reflection
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type factoryType = assembly
+                .GetTypes()
+                .FirstOrDefault(t => typeof(Vehicle).IsAssignableFrom(t) && !t.IsAbstract && t.Name == type);
+
+            if (factoryType==null)
             {
-                case "Semi":
-                    return new Semi();
-                case "Truck":
-                    return new Truck();
-                case "Van":
-                    return new Van();
-                default:
-                    throw new InvalidOperationException("Invalid vehicle type!");
+                throw new InvalidOperationException("Invalid vehicle type!");
             }
+
+            Vehicle vehicle = (Vehicle)Activator.CreateInstance(factoryType);
+            return vehicle;
         }
     }
 }
