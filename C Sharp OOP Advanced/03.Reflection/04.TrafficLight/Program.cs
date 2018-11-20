5e44929c496f69d5ae121c2c1f13f049bd0f7394
Hristo <hri100atanasov@gmail.com>
2018-11-20 18:05:36 +0200

    update

---
+++ C Sharp OOP Advanced/03.Reflection/04.TrafficLight/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _04.TrafficLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var three = 3;
+            var input = Console.ReadLine().Split();
+            var currentSignal = input[2];
+            var count = int.Parse(Console.ReadLine());
+            var generatedSignal = new string[3];
+
+
+            while (three-- > 0)
+            {
+                var index = 0;
+                Console.WriteLine(Enum.GetName(typeof(Signals), currentSignal));
+                index++;
+            }
+
+            foreach (var item in generatedSignal)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ C Sharp OOP Advanced/03.Reflection/04.TrafficLight/Signals.cs
@@ -0,0 +1,9 @@
+﻿namespace _04.TrafficLight
+{
+    public enum Signals
+    {
+        Red,
+        Green,
+        Yellow
+    }
+}
