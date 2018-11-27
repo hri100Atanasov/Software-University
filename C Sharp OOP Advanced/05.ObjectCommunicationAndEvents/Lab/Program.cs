1267f3a35e4358c5c8285a71ca3b39d7a1f44584
Hristo Atanasov <hri100atanasov@gmail.com>
2018-11-27 18:48:34 +0200

    update

--- C Sharp OOP Advanced/05.ObjectCommunicationAndEvents/Lab/Program.cs
+++ C Sharp OOP Advanced/05.ObjectCommunicationAndEvents/Lab/Program.cs
@@ -4,9 +4,31 @@ namespace Lab
 {
     class Program
     {
+        public delegate void DelegateToPrint(string stringToPrint);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stringToPrint = "Iko";
+            DelegateToPrint dp = PrintToConsole1;
+            dp += PrintToConsole2;
+            dp += PrintToConsole3;
+            dp.GetInvocationList();
+            dp(stringToPrint);
+        }
+
+        private static void PrintToConsole1(string stringToPrint)
+        {
+            Console.WriteLine("1: "+stringToPrint);
+        }
+
+        private static void PrintToConsole2(string stringToPrint)
+        {
+            Console.WriteLine("2: " + stringToPrint);
+        }
+
+        private static void PrintToConsole3(string stringToPrint)
+        {
+            Console.WriteLine("3: " + stringToPrint);
         }
     }
 }
