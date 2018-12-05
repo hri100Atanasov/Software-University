bac1cab01d4709a6fc62afcb2faffd893a66021b
Hristo Atanasov <hri100atanasov@gmail.com>
2018-12-05 23:18:37 +0200

    First problem solution

---
+++ Databases Advanced - Entity Framework/01. IntroductionToEntityFramework/01. Employees Full Information/Data/Models/EmployeesProject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._IntroductionToEntityFramework.Data.Models
+{
+    public partial class EmployeesProject
+    {
+        public int EmployeeId { get; set; }
+        public int ProjectId { get; set; }
+
+        public virtual Employee Employee { get; set; }
+        public virtual Project Project { get; set; }
+    }
+}
---
+++ Databases Advanced - Entity Framework/01. IntroductionToEntityFramework/01. Employees Full Information/Program.cs
@@ -0,0 +1,22 @@
+﻿using _01._IntroductionToEntityFramework.Data;
+using System.IO;
+using System.Linq;
+
+namespace _01._IntroductionToEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SoftUniContext())
+            {
+                using (var sw = new StreamWriter("../Exercise.txt"))
+                {
+                    context.Employees.OrderBy(e => e.EmployeeId)
+                         .Select(e => new { e.FirstName, e.MiddleName, e.LastName, e.JobTitle, e.Salary }).ToList()
+                         .ForEach(e => sw.WriteLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}"));
+                }
+            }
+        }
+    }
+}
