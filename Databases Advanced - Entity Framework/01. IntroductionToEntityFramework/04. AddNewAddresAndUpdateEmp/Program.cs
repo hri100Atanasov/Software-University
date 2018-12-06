1a4d940a483dcf6515ace37d8ea142dc1078304c
Hristo Atanasov <hri100atanasov@gmail.com>
2018-12-06 20:42:12 +0200

    solved problem 4 and 5

---
+++ Databases Advanced - Entity Framework/01. IntroductionToEntityFramework/04. AddNewAddresAndUpdateEmp/Program.cs
@@ -0,0 +1,32 @@
+﻿using _01._IntroductionToEntityFramework.Data;
+using _01._IntroductionToEntityFramework.Data.Models;
+using System;
+using System.Linq;
+
+namespace _04._AddNewAddresAndUpdateEmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SoftUniContext())
+            {
+                var address = new Address()
+                {
+                    AddressText = "Vitoshka 15",
+                    TownId = 4
+                };
+
+                context.Addresses.Add(address);
+                context.Employees.FirstOrDefault(e => e.LastName == "Nakov").Address = address;
+                context.SaveChanges();
+
+                context.Employees.OrderByDescending(e => e.Address.AddressId)
+                    .Take(10)
+                    .Select(e => e.Address.AddressText)
+                    .ToList()
+                    .ForEach(e => Console.WriteLine(e));
+            }
+        }
+    }
+}
---
+++ Databases Advanced - Entity Framework/01. IntroductionToEntityFramework/05.EmployeesAndProjects/Program.cs
@@ -0,0 +1,32 @@
+﻿using _01._IntroductionToEntityFramework.Data;
+using System;
+using System.Linq;
+
+namespace _05.EmployeesAndProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dbContext = new SoftUniContext())
+            {
+                dbContext.Employees
+                    .Where(e => e.EmployeesProjects
+                        .Any(ep => ep.Project.StartDate.Year >= 2001 && ep.Project.StartDate.Year <= 2003))
+                    .Take(30)
+                    .Select(e => new
+                    {
+                        e.FirstName,
+                        e.LastName,
+                        ManagerFirstName = e.Manager.FirstName,
+                        ManagerLastName = e.Manager.LastName,
+                        Projects = e.EmployeesProjects
+                            .Select(ep => ep.Project)
+                    })
+                    .ToList()
+                    .ForEach(e => Console.WriteLine($"{e.FirstName} {e.LastName} - Manager: {e.ManagerFirstName} {e.ManagerLastName}{Environment.NewLine}" +
+                    $"{String.Join(Environment.NewLine, e.Projects.Select(p => $"--{p.Name} - {p.StartDate.ToString()} - {(p.EndDate == null ? "not finished" : p.EndDate.ToString())}"))}"));
+            }
+        }
+    }
+}
