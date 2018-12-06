ae27255a256979b30b0edaec3cfcf9c649a7954f
Hristo Atanasov <hri100atanasov@gmail.com>
2018-12-06 11:57:19 +0200

    update

---
+++ Databases Advanced - Entity Framework/01. IntroductionToEntityFramework/03. EmployeesResDev/Program.cs
@@ -0,0 +1,25 @@
+﻿using _01._IntroductionToEntityFramework.Data;
+using System.IO;
+using System.Linq;
+
+namespace _03._EmployeesResDev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SoftUniContext())
+            {
+                using (var sw = new StreamWriter("../../SoftUni.txt"))
+                {
+                    context.Employees.Where(e => e.Department.Name == "Research and Development")
+                        .OrderBy(e => e.Salary)
+                        .ThenByDescending(e => e.FirstName)
+                        .Select(e => new { e.FirstName, e.LastName, departmentName = e.Department.Name, e.Salary })
+                        .ToList()
+                        .ForEach(e => sw.WriteLine($"{e.FirstName} {e.LastName} from {e.departmentName} - ${e.Salary:f2}"));
+                }
+            }
+        }
+    }
+}
